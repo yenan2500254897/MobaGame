847d4e544d364708e58fe1e804e06ac9c67aeb12
yenan2500254897 <2500254897@qq.com>
2021-01-14 14:56:29 +0800

    角色技能枚举类

---
+++ Source/MobaGame/MobaGameType.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+enum class ESkillKeyType : uint8
+{
+	KEY_Q,
+	KEY_W,
+	KEY_E,
+	KEY_R,
+	KEY_MAX
+};
